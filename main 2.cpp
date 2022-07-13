12e6f7af78397fc96251c68687a89fb84234dfa8
Vikram chaurasiya <41546109+mahakal123@users.noreply.github.com>
2022-07-13 23:39:24 +0530

    Add files via upload

---
+++ main 2.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int
+main ()
+{
+  char c;
+  cout << "enter the charector" << endl;
+  cin >> c;
+  switch (c)
+    {
+    case 'a':
+      cout << "vowel";
+      break;
+    case 'e':
+      cout << "vowel";
+      break;
+    case 'i':
+      cout << "vowel";
+      break;
+    case 'o':
+      cout << "vowel";
+      break;
+    case 'u':
+      cout << "vowel";
+      break;
+    case 'A':
+      cout << "vowel";
+      break;
+    case 'E':
+      cout << "vowel";
+      break;
+    case 'I':
+      cout << "vowel";
+      break;
+    case 'O':
+      cout << "vowel";
+      break;
+    case 'U':
+      cout << "vowel";
+      break;
+    default:
+      cout << "CONSONANT";
+      break;
+    }
+return 0;
+}
