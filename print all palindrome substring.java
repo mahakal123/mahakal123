40c2ec328bac491875fd61084d82736d1e2edfba
Vikram chaurasiya <41546109+mahakal123@users.noreply.github.com>
2022-07-18 22:04:41 +0530

    Create print all palindrome substring.java

---
+++ print all palindrome substring.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static boolean ispalindrome(String s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            char ch1=s.charAt(i);
+            char ch2=s.charAt(j);
+            if(ch1!=ch2){
+                return false;
+            }else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+    } 
+   
+	public static void solution(String str){
+		for(int i=0;i<str.length();i++){
+		    for(int j=i+1;j<=str.length();j++){
+		        String ss=str.substring(i, j);
+		        if(ispalindrome(ss)==true){
+		            System.out.println(ss);
+		        }
+		    }
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		solution(str);
+	}
+
+}
