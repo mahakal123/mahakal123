fb78830a1a7668e31f6b40b81ab9a0004d86cbe5
Vikram chaurasiya <41546109+mahakal123@users.noreply.github.com>
2022-07-25 14:03:57 +0530

    Create getkeypadcombination.java

---
+++ getkeypadcombination.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        String str=scn.next();
+        ArrayList<String> words=getKPC(str);
+        System.out.println(words);
+    }
+static String[] codes = {",;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+    public static ArrayList<String> getKPC(String str) {
+        if(str.length()==0){
+            ArrayList<String> bres=new ArrayList<>();
+            bres.add("");
+            return bres;
+        }
+        char ch=str.charAt(0);
+        String ros=str.substring(1);
+        ArrayList<String> rres=getKPC(ros);
+        ArrayList<String> myres=new ArrayList<>();
+
+        String codeforch=codes[ch-'0'];
+        for(int i=0;i<codeforch.length();i++){
+            char chcode=codeforch.charAt(i);
+            for(String rstr:rres){
+                myres.add(chcode + rstr);
+            }
+        }
+
+
+        return myres;
+    }
+
+}
