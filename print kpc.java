9524f8f41d8156f43cfa78280400ffdbc0fd70e5
Vikram chaurasiya <41546109+mahakal123@users.noreply.github.com>
2022-07-26 12:36:22 +0530

    Create print kpc.java

---
+++ print kpc.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        String str=scn.next();
+        printKPC(str,"");
+
+    }
+    static String[]codes={".;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+    public static void printKPC(String str, String ans) {
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+
+        char ch=str.charAt(0);
+        String ros=str.substring(1);
+
+        String codeforch=codes[ch-'0'];
+        for(int i=0;i<codeforch.length();i++){
+            char cho=codeforch.charAt(i);
+            printKPC(ros,ans+cho);
+        }
+
+
+    }
+
+}
